6327866bb3a05924409c7b4c03c87836b9709fa7
ava <ava239@yandex.ru>
2020-08-08 22:49:07 +0300

    added .yaml extension

--- src/Parsers.php
+++ src/Parsers.php
@@ -9,6 +9,7 @@ function parse($data, $format)
     switch ($format) {
         case 'json':
             return json_decode($data, true);
+        case 'yaml':
         case 'yml':
             $object = Yaml::parse($data, Yaml::PARSE_OBJECT_FOR_MAP);
             $object = convertToArray($object);
