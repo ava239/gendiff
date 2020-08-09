97e0fb425cbfd65dee7c1f14dff81f347e63b827
ava <ava239@yandex.ru>
2020-08-09 04:04:22 +0300

    refactored compare function

--- src/Core.php
+++ src/Core.php
@@ -9,30 +9,28 @@ function compare($a, $b)
     $changed1 = array_filter(array_keys($a), fn($x) => !in_array($x, $added) && !in_array($x, $removed));
     $changed2 = array_filter(array_keys($b), fn($x) => !in_array($x, $added) && !in_array($x, $removed));
     $changed = array_unique(array_merge($changed1, $changed2));
-    $diff = array_reduce($changed, function ($acc, $i) use ($a, $b) {
-        if (is_array($a[$i]) || $a[$i] === $b[$i]) {
-            $acc['kept'][$i] = innerCompare($a[$i], $b[$i]);
+    $innerCompare = function ($a, $b) {
+        if (is_array($a) && is_array($b)) {
+            return compare($a, $b);
         } else {
-            $acc['changed'][$i] = [innerCompare($a[$i], $b[$i]), innerCompare($b[$i], $a[$i])];
+            return $a;
+        }
+    };
+    $diff = array_reduce($changed, function ($acc, $i) use ($a, $b, $innerCompare) {
+        if (is_array($a[$i]) || $a[$i] === $b[$i]) {
+            $acc['kept'][$i] = $innerCompare($a[$i], $b[$i]);
+        } else {
+            $acc['changed'][$i] = [$innerCompare($a[$i], $b[$i]), $innerCompare($b[$i], $a[$i])];
         }
         return $acc;
     }, ['kept' => [], 'changed' => [], 'removed' => [], 'added' => []]);
-    $diff = array_reduce($added, function ($acc, $i) use ($a, $b) {
-        $acc['added'][$i] = innerCompare($b[$i], $b[$i]);
+    $diff = array_reduce($added, function ($acc, $i) use ($a, $b, $innerCompare) {
+        $acc['added'][$i] = $innerCompare($b[$i], $b[$i]);
         return $acc;
     }, $diff);
-    $diff = array_reduce($removed, function ($acc, $i) use ($a, $b) {
-        $acc['removed'][$i] = innerCompare($a[$i], $a[$i]);
+    $diff = array_reduce($removed, function ($acc, $i) use ($a, $b, $innerCompare) {
+        $acc['removed'][$i] = $innerCompare($a[$i], $a[$i]);
         return $acc;
     }, $diff);
     return $diff;
-}
-
-function innerCompare($e, $e2)
-{
-    if (is_array($e) && is_array($e2)) {
-        return compare($e, $e2);
-    } else {
-        return $e;
-    }
 }
